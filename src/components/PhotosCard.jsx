a468ec1c19c784df54406945e5552e6e030f6446
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-28 17:13:43 +0600

    top generations image card done

--- src/components/PhotosCard.jsx
+++ src/components/PhotosCard.jsx
@@ -1,4 +1,4 @@
-import { Separator } from '@heroui/react';
+import { Button, Chip, Separator } from '@heroui/react';
 import Image from 'next/image';
 import React from 'react';
 import { IoHeart } from 'react-icons/io5';
@@ -6,14 +6,18 @@ import { PiDownloadSimpleLight } from 'react-icons/pi';
 
 const PhotosCard = ({ photo }) => {
     return (
-        <div>
-            {
+        <div className='rounded-xl p-5 shadow-sm border'>
+
+            <div className='relative w-full aspect-square'>
                 <Image
-                    width={500}
-                    height={500}
+                    className='rounded-xl object-cover'
+                    fill
+                    sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
                     src={photo.imageUrl}
                     alt='title' />
-            }
+                    <Chip size='sm' className='absolute right-2 top-2'>{photo.category}</Chip>
+            </div>
+
             <div className='font-medium text-2xl my-3'>
                 <h2>{photo.title}</h2>
             </div>
@@ -28,6 +32,7 @@ const PhotosCard = ({ photo }) => {
                     <p>{photo.downloads}</p>
                 </div>
             </div>
+            <Button className='w-full' variant='outline'>View</Button>
         </div>
     );
 };
