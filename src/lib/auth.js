ef5c77623fea1605e6a44449d50dd1c1904f7b50
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-29 12:09:05 +0600

    sign up authentication setup successfull

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,3 +1,6 @@
+import dns from 'node:dns';
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
@@ -10,7 +13,7 @@ export const auth = betterAuth({
         enabled: true,
     },
     database: mongodbAdapter(db, {
-        // Optional: if you don't provide a client, database transactions won't be enabled.
+
         client
     }),
 });
