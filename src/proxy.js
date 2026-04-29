0dbfb8a7782fd72f2c5c1e88f01fbbd126137b0f
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-29 17:09:44 +0600

    proxy setup done

---
+++ src/proxy.js
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server'
+import { auth } from './lib/auth'
+import { headers } from "next/headers";
+export async function proxy(request) {
+
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+
+    if (!session) {
+        return NextResponse.redirect(new URL('/signin', request.url))
+    }
+}
+
+
+export const config = {
+    matcher: ['/profile', '/all-photos', '/all-photos/:path'],
+}
