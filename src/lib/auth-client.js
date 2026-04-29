0d78c10313839f44898fa12a227bd9802221fb9f
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-29 17:20:11 +0600

    auth client update

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({  
-    baseURL: "http://localhost:3000"
+    baseURL: "https://pixgen-ai-kappa.vercel.app"
 })
 
 export const { signIn, signUp, useSession } = createAuthClient()
